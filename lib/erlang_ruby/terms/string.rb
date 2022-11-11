8271cb958ca5907df6b1515e87ddf5f84357b584
DanilMayorow <alexbearning@gmail.com>
2022-11-11 17:25:07 +0700

    Add implementation for string types

--- lib/erlang_ruby/terms/string.rb
+++ lib/erlang_ruby/terms/string.rb
@@ -7,11 +7,25 @@ module ErlangRuby
       end
 
       def decode
-        value.to_sym
+        value
       end
 
       def self.build(data)
-        puts data
+        dup_data = data.dup
+        tag, rest_of_data = Utils.read_first(dup_data)
+        case tag
+        when STRING
+          length = dup_data.byteslice(0, 2).unpack("n").first
+          content = dup_data.byteslice(2, length).unpack("C*").first
+        when BINARY
+          length = dup_data.byteslice(0, 4).unpack("N").first
+          content = dup_data.byteslice(4, length).unpack("A*").first
+        else
+          raise "Undefined type of atom"
+        end
+
+        str = self.new(content)
+        [str, rest_of_data[length..-1]]
       end
     end
   end
